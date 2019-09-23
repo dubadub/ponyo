eb7015a07e6deacaf5190b377d51225393ecb1b7
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2019-09-23 19:05:56 +0100

    Use classes

--- main.py
+++ main.py
@@ -18,11 +18,101 @@ def init_board(ponyo_position, shark_position, my_board):
 boardsize = 50        # board will be X by X where X = boardsize
 
 # Initialize the board
-ponyo_position = [45, 5]
+ponyo_position = [25, 25]
 shark_position = [5, 45]
 epoch = 0
 my_board = np.zeros((boardsize, boardsize))
 my_board = init_board(ponyo_position, shark_position, my_board)
+
+class Ponyo:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, delta_x, delta_y):
+        self.x = self.x + delta_x
+        self.y = self.y + delta_y
+
+
+class Shark:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, delta_x, delta_y):
+        self.x = self.x + delta_x
+        self.y = self.y + delta_y
+
+
+class Board:
+
+    def __init__(self, size):
+        self.size = size
+        self.values = np.zeros((size, size))
+
+    def set(self, x, y, value):
+        self.values[x,y] = value
+
+
+class Game:
+
+    def __init__(self, board, ponyo, shark):
+        self.ponyo = ponyo
+        self.shark = shark
+        self.board = board
+        self.finished = False
+        self.frame = 0
+
+
+    def generator(self):
+        while not self.finished:
+            self.frame += 1
+            yield self.frame
+
+    def tick(self):
+        self.move_ponyo()
+        self.move_shark()
+
+        if self.shark.x == self.ponyo.x and self.shark.y == self.ponyo.y:
+            self.finished = True
+
+    def move_ponyo(self):
+        delta_x = random.randint(-3, 3)
+        delta_y = random.randint(-3, 3)
+
+        if self.ponyo.x + delta_x < 0 or self.ponyo.x + delta_x >= self.board.size:
+            delta_x = 0
+
+        if self.ponyo.y + delta_y < 0 or self.ponyo.y + delta_y >= self.board.size:
+            delta_y = 0
+
+        ponyo_position_new = [self.ponyo.x + delta_x, self.ponyo.y + delta_y]
+
+        self.board.set(ponyo_position_new[0], ponyo_position_new[1], 1)
+        self.board.set(self.ponyo.x, self.ponyo.y, 0)
+
+        self.ponyo.x = ponyo_position_new[0]
+        self.ponyo.y = ponyo_position_new[1]
+
+    def move_shark(self):
+        delta_x = 1 if self.ponyo.x - self.shark.x > 0 else -1
+        delta_y = 1 if self.ponyo.y - self.shark.y > 0 else -1
+
+        if self.shark.x + delta_x < 0 or self.shark.x + delta_x >= self.board.size:
+            delta_x = 0
+
+        if self.shark.y + delta_y < 0 or self.shark.y + delta_y >= self.board.size:
+            delta_y = 0
+
+        shark_position_new = [self.shark.x + delta_x, self.shark.y + delta_y]
+
+        self.board.set(shark_position_new[0], shark_position_new[1], 2)
+        self.board.set(self.shark.x, self.shark.y, 0)
+
+        self.shark.x = shark_position_new[0]
+        self.shark.y = shark_position_new[1]
 
 ##### Animate the board #####
 
@@ -31,72 +121,17 @@ fig = plt.gcf()
 # Show first image - which is the initial board
 im = plt.imshow(my_board)
 
-def move_ponyo():
-    delta_x = random.randint(-3, 3)
-    delta_y = random.randint(-3, 3)
-
-    if ponyo_position[0] + delta_x < 0 or ponyo_position[0] + delta_x >= boardsize:
-        delta_x = 0
-
-    if ponyo_position[1] + delta_y < 0 or ponyo_position[1] + delta_y >= boardsize:
-        delta_y = 0
-
-    ponyo_position_new = [ponyo_position[0] + delta_x, ponyo_position[1] + delta_y]
-
-    my_board[ponyo_position_new[0], ponyo_position_new[1]] = 1
-    my_board[ponyo_position[0], ponyo_position[1]] = 0
-
-    ponyo_position[0] = ponyo_position_new[0]
-    ponyo_position[1] = ponyo_position_new[1]
-
-def move_shark():
-    delta_x = 1 if ponyo_position[0] - shark_position[0] > 0 else -1
-    delta_y = 1 if ponyo_position[1] - shark_position[1] > 0 else -1
-
-    if shark_position[0] + delta_x < 0 or shark_position[0] + delta_x >= boardsize:
-        delta_x = 0
-
-    if shark_position[1] + delta_y < 0 or shark_position[1] + delta_y >= boardsize:
-        delta_y = 0
-
-    shark_position_new = [shark_position[0] + delta_x, shark_position[1] + delta_y]
-
-    my_board[shark_position_new[0], shark_position_new[1]] = 2
-    my_board[shark_position[0], shark_position[1]] = 0
-
-    shark_position[0] = shark_position_new[0]
-    shark_position[1] = shark_position_new[1]
-
-finished = False
-
-def gen():
-    global finished
-    global epoch
-
-    while not finished:
-        epoch += 1
-        yield epoch
-
-def update_board():
-    global finished
-
-    move_ponyo()
-    move_shark()
-
-    if shark_position[0] == ponyo_position[0] and shark_position[1] == ponyo_position[1]:
-        finished = True
-
-    return my_board
-
-
+game = Game(Board(50), Ponyo(25,25), Shark(5, 45))
 # Helper function that updates the board and returns a new image of
 # the updated board animate is the function that FuncAnimation calls
 def update(frame):
-    im.set_data(update_board())
-    global epoch
-    plt.title(epoch)
+    game.tick()
+    im.set_data(game.board.values)
+
+    plt.title(game.frame)
     return im,
 
 
-ani = animation.FuncAnimation(fig, update, frames=gen, blit=True, repeat = False)
+ani = animation.FuncAnimation(fig, update, frames=game.generator, blit=True, repeat = False)
 plt.show()
+
